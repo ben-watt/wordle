73e9843a614267d3cb51acf6de2e354329d3ae39
ben.watt <ben.watt@moonpig.com>
2022-01-21 10:41:16 +0000

    Update about modal

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -61,19 +61,12 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
                   </Dialog.Title>
                   <div className="mt-2">
                     <p className="text-sm text-gray-500">
-                      Created by
-                      <a
-                        href="https://github.com/ben-watt"
-                        className="underline font-bold"
-                      >
-                        Ben Watt and Katie Vallance.
-                      </a>{' '} 
-                      With a big thanks to the original wordle and the creator of the open source wordle fork.{' '}
+                      A big thanks to the original wordle and the creator of the open source wordle clone.{' '}
                       <a
                         href="https://github.com/hannahcode/wordle"
                         className="underline font-bold"
                       >
-                        Check out the fork here
+                        Check out the clone here
                       </a>{' '}
                       and{' '}
                       <a
